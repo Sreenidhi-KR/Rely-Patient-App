5e597af51abbfb075cbb2f636e09c94dbffa0ef9
Aditya M <aditya_gowda_2000@yahoo.com>
2023-03-30 18:16:28 +0530

    added documents screen

--- src/screens/user/DocumentScreen.js
+++ src/screens/user/DocumentScreen.js
@@ -5,7 +5,7 @@ import {
   Text,
   View,
 } from "react-native";
-import { List, Button, FAB } from "react-native-paper";
+import { List, Button, FAB, Appbar } from "react-native-paper";
 import React, { useState, useEffect } from "react";
 import {
   uploadDocument,
@@ -18,71 +18,83 @@ import {
 // create a component
 const DocumentScreen = () => {
   const [docs, setDocs] = useState([]);
+  const [isLoading, setLoading] = useState(true);
   //onMount load all the documents
   useEffect(() => {
     getDocuments();
+    docsForConsultation();
   }, []);
 
   async function getDocuments() {
     data = await getAllDocuments();
     setDocs(data);
-    console.log(docs);
+    setLoading(false);
   }
 
   async function docUpload() {
+    setLoading(true);
     await uploadDocument();
     await getDocuments();
   }
 
   async function removeDoc(docId) {
+    setLoading(true);
     await removeDocument(docId);
     await getDocuments();
   }
 
   async function downloadDoc(docId) {
+    setLoading(true);
     await downloadDocument(item.id);
     await getDocuments();
   }
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Documents</Text>
-      <FlatList
-        data={docs}
-        keyExtractor={({ id }) => id}
-        renderItem={({ item }) => (
-          <View style={styles.box}>
-            <List.Item
-              title={`${item.name}`}
-              titleStyle={{ color: "black" }}
-              right={() => {
-                return (
-                  <View
-                    style={{
-                      flex: 1,
-                      flexDirection: "row",
-                      justifyContent: "flex-end",
-                    }}
-                  >
-                    <Button
-                      icon="download"
-                      onPress={() => {
-                        downloadDocument(item.id);
-                      }}
-                    ></Button>
-                    <Button
-                      icon="delete"
-                      onPress={() => {
-                        removeDoc(item.id);
-                      }}
-                    ></Button>
-                  </View>
-                );
-              }}
-            />
-          </View>
-        )}
-      />
-
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <View>
+          <Appbar.Header style={{ backgroundColor: "#F5ECFF" }}>
+            <Appbar.Content title="Documents" titleStyle={{ color: "black" }} />
+          </Appbar.Header>
+          <FlatList
+            data={docs}
+            keyExtractor={({ id }) => id}
+            renderItem={({ item }) => (
+              <View style={styles.box}>
+                <List.Item
+                  title={`${item.name}`}
+                  titleStyle={{ color: "black" }}
+                  right={() => {
+                    return (
+                      <View
+                        style={{
+                          flex: 1,
+                          flexDirection: "row",
+                          justifyContent: "flex-end",
+                        }}
+                      >
+                        <Button
+                          icon="download"
+                          onPress={() => {
+                            downloadDocument(item.id);
+                          }}
+                        ></Button>
+                        <Button
+                          icon="delete"
+                          onPress={() => {
+                            removeDoc(item.id);
+                          }}
+                        ></Button>
+                      </View>
+                    );
+                  }}
+                />
+              </View>
+            )}
+          />
+        </View>
+      )}
       <FAB
         icon="plus"
         style={styles.fab}
