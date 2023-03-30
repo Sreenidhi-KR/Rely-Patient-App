ced278ffcec92e8d7ef91b8cddabab8a9c078a18
Aditya M <aditya_gowda_2000@yahoo.com>
2023-03-30 16:18:40 +0530

    added delete functionality

--- src/screens/user/DocumentScreen.js
+++ src/screens/user/DocumentScreen.js
@@ -1,24 +1,100 @@
-import { View, Text, Button } from "react-native";
-import React, { useState } from "react";
-import { uploadDocument } from "../../service/DocumentService";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  FlatList,
+  Text,
+  View,
+} from "react-native";
+import { List, Button } from "react-native-paper";
+import React, { useState, useEffect } from "react";
+import {
+  uploadDocument,
+  getAllDocuments,
+  removeDocument,
+  downloadDocument,
+  docsForConsultation,
+} from "../../service/DocumentService";
 
 // create a component
 const DocumentScreen = () => {
+  const [docs, setDocs] = useState([]);
+  //onMount load all the documents
+  useEffect(() => {
+    getDocuments();
+  }, []);
+
+  async function getDocuments() {
+    data = await getAllDocuments();
+    setDocs(data);
+    console.log(docs);
+  }
+
+  async function removeDoc(docId) {
+    await removeDocument(docId);
+    await getDocuments();
+  }
+
+  async function downloadDoc(docId) {
+    await downloadDocument(item.id);
+    await getDocuments();
+  }
   return (
-    <View style={{ flex: 1 }}>
-      {/* list all the files which are already uploded */}
-      <View style={{ flex: 5, justifyContent: "flex-start" }}>
-        <Text>Display all the previously uploded files</Text>
-      </View>
-      {/* upload new file */}
-      <View style={{ flex: 1, justifyContent: "flex-end" }}>
-        <Button
-          title="Upload Document"
-          onPress={() => uploadDocument()}
-        ></Button>
-      </View>
+    <View>
+      <Text style={styles.text}>Documents</Text>
+      <FlatList
+        data={docs}
+        keyExtractor={({ id }) => id}
+        renderItem={({ item }) => (
+          <View style={styles.box}>
+            <List.Item
+              title={`${item.name}`}
+              titleStyle={{ color: "black" }}
+              right={() => {
+                return (
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "row",
+                      justifyContent: "flex-end",
+                    }}
+                  >
+                    <Button
+                      icon="download"
+                      onPress={() => {
+                        downloadDocument(item.id);
+                      }}
+                    ></Button>
+                    <Button
+                      icon="delete"
+                      onPress={() => {
+                        removeDoc(item.id);
+                      }}
+                    ></Button>
+                  </View>
+                );
+              }}
+            />
+          </View>
+        )}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    flexDirection: "column",
+  },
+  box: {
+    height: 100,
+    width: "90%",
+    padding: 10,
+    margin: 10,
+    borderRadius: 20,
+    backgroundColor: "#F5ECFF",
+  },
+});
 //make this component available to the app
 export default DocumentScreen;
