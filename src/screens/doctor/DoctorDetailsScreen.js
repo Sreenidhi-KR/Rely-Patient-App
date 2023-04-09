af072112c5d7d3883a3e1d46c8be03dbb4432e79
Kiran Kumar B <kiran0317@gmail.com>
2023-04-09 20:01:00 +0530

    Updated doctor details

--- src/screens/doctor/DoctorDetailsScreen.js
+++ src/screens/doctor/DoctorDetailsScreen.js
@@ -1,7 +1,7 @@
 //import liraries
 import React, { Component } from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
-import { Button, Badge } from "react-native-paper";
+import { Button, Chip } from "react-native-paper";
 import routes from "../../navigation/routes";
 
 // create a component
@@ -9,26 +9,30 @@ const DoctorDetailsScreen = ({ navigation, route }) => {
   const { doctor } = route.params;
   console.log(doctor);
   const imageUrl = doctor.photo_url;
-  const description = "Hi I'm an experienced and highly qualified physician with a passion for providing top-notch medical care to patients, I believe in open and honest communication with patients to achieve the best possible health outcomes."
   return (
     <View style={styles.container}>
       <View style={styles.image}>
-      <Image
-          source={imageUrl ? {uri: imageUrl} : require(`../../../assets/general-doc.png`)}
-          style={{ width: 100, height: 100 }}/>
+          <Image
+              source={imageUrl ? {uri: imageUrl} : require(`../../../assets/general-doc.png`)}
+              style={{ width: 100, height: 100 , marginRight:50 }}/>
           <View style={styles.details}>
             <Text style={{fontSize:24, fontStyle:"bold"}}>Dr.{doctor.fname} {doctor.lname}</Text>
             <Text style={styles.text}>{doctor.qualification}</Text>
             <Text style={styles.text}>{doctor.email}</Text>
+            <Text style={styles.text}>Sex: {doctor.sex}    Age:{doctor.age}</Text> 
             <View style={styles.detail}>
-            <Badge style={styles.textdec}>{doctor.specialization}</Badge>
-            <Badge style={styles.textdec}>Rating: {doctor.rating}/5</Badge>
+            <Chip style={styles.textdec} compact>{doctor.specialization}</Chip>
+            <Chip style={styles.textdec} compact>Rating: {doctor.rating}/5</Chip>
           </View>
           </View>
       </View>
-      
       <View style={styles.description}>
-        <Text style={{fontSize: 16, fontFamily: "Sans-sherif"}}>{description}</Text>
+        <Text style={{fontSize: 16, fontFamily: "Sans-sherif", textAlign: "justify"}}>{doctor.description}</Text>
+      </View>
+      <View style={styles.info}>
+        <Text style={{fontSize: 16, fontFamily: "Sans-sherif", textAlign: "justify"}}>{doctor.clinic_address}{'\n'}</Text>
+        <Text style={{fontSize: 16, fontFamily: "Sans-sherif", textAlign: "justify", lineHeight: 15}}>Location: {doctor.city}, {doctor.state}</Text>
+        <Text style={{fontSize: 16, fontFamily: "Sans-sherif"}}>Timings: {doctor.available_timings}</Text>
       </View>
       <Button style={styles.button}
         mode="contained"
@@ -50,35 +54,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
-    alignItems: "center",
+    // alignItems: "center",
     backgroundColor: "white",
-    fontSize: 25,
+    
   },
   image:{
     flexDirection:"row",
-    flexWrap: "wrap",
-    alignItems: "stretch",
-    marginStart: "-5%",
-    marginTop: "10%",
-    marginRight: "-5%",
-    // marginBottom: "5%",
+    alignItems: "center",
+    justifyContent: "space-around",
+    margin:10,
     backgroundColor: "#F5ECFF",
     borderRadius: 10,
-    padding: "3%",
+    padding: 20,
   },
   details: {
     flexDirection: "column",
-    marginStart: "8%",
-    marginRight: "5%",
   },
   detail:{
     flexDirection: "row",
     justifyContent: "space-around",
-    marginVertical: "5%",
-    // paddingEnd: "5%"
+    marginTop: 10,
   },
   text: {
-    fontSize:16,
+    fontSize:14,
     fontStyle:"bold", 
   },
   textdec: {
@@ -86,25 +84,23 @@ const styles = StyleSheet.create({
      fontSize: 14,
      fontStyle: "bold",
      color: "black",
-     size: 25,
      marginEnd: 15,
   },
   description: {
     justifyContent: "center",
     alignItems: "center",
-    marginTop: "5%",
-    marginRight: "1%",
-    // marginBottom: "5%",
-    // backgroundColor: "#F5ECFF",
-    borderRadius: 10,
+    marginTop: "1%",
+    marginHorizontal: "1%",
     padding: "5%",
-    color: "black",
-    fontSize: 24,
   },
   button: {
-    // justifyContent: "center",
     marginTop: "15%",
     alignItems: "center",
+    marginHorizontal: 100
+  },
+  info: {
+    marginHorizontal: "6%",
+    justifyContent: "space-evenly"
   }
 });
 
