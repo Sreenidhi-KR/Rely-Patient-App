4f92ec41a24a2ebbdac41d0af3a06c69f412e029
Aditya M <aditya_gowda_2000@yahoo.com>
2023-04-25 22:17:44 +0530

    patient profile picture update

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,10 +1,56 @@
 import axios from "axios";
-import { BASE_URL, getConfig } from "../config";
+import { BASE_URL, getConfig, getToken } from "../config";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import DocumentPicker from "react-native-document-picker";
 import Toast from "react-native-simple-toast";
 
 const urlBase = `${BASE_URL}/api/v1`;
 
+async function downloadPhoto(patientId) {
+  const config = {
+    method: "POST",
+    headers: {
+      "ngrok-skip-browser-warning": "true",
+      Authorization: `Bearer ${await getToken()}`,
+    },
+  };
+  let response = await axios.get(
+    `${urlBase}/user/downloadImage/${patientId}`,
+    config
+  );
+  return response.data;
+}
+async function uploadPhoto(patientId) {
+  const config = {
+    method: "POST",
+    headers: {
+      "ngrok-skip-browser-warning": "true",
+      Authorization: `Bearer ${await getToken()}`,
+      "Content-Type": "multipart/form-data",
+    },
+  };
+  try {
+    const doc = await DocumentPicker.pick({
+      type: [DocumentPicker.types.images],
+    });
+    const formdata = new FormData();
+
+    formdata.append("image", {
+      uri: doc[0].uri,
+      type: doc[0].type,
+      name: doc[0].name,
+      size: doc[0].size,
+    });
+
+    let response = await axios.post(
+      `${urlBase}/user/uploadImage/${patientId}`,
+      formdata,
+      config
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
 async function getProfilesForUser() {
   try {
     let userInfo = await AsyncStorage.getItem("userInfo");
@@ -17,8 +63,8 @@ async function getProfilesForUser() {
     return response.data;
   } catch (err) {
     console.log(err);
-    Toast.show("Unable to fetch profiles for User", 10);    
+    Toast.show("Unable to fetch profiles for User", 10);
   }
 }
 
-export { getProfilesForUser };
+export { getProfilesForUser, uploadPhoto, downloadPhoto };
